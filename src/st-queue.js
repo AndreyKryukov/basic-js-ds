f26173a9d91cf73a7cfc30787e640b5730f07cc4
AndreyKryukov <ander.kryukov@yandex.ru>
2021-10-19 01:07:24 +0300

    feat: solved the problem of Remove from list, Stack and Queue.

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,33 +1,37 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
 module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.list = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.list;
+  }
+
+  enqueue(value) {
+    let following = this.list;
+
+    if (!this.list) {
+      return this.list = new ListNode(value);
+    }
+
+    while (following) {
+      if (!following.next) {
+        return following.next = new ListNode(value);
+      } else {
+        following = following.next;
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let initialValue = this.list.value;
 
+    if (!this.list) return null;
+    this.list.value = this.list.next.value;
+    this.list.next = this.list.next.next;
+    return initialValue;
+  }
 }
